8004050877d9cd9ba9d715a1347e887c337231e5
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-04-02 15:15:29 +0900

    Add files via upload

---
+++ PyQt/7.PyQt5 Signal&Slot/7-4.py
@@ -0,0 +1,37 @@
+# http://codetorial.net/pyqt5/signal_slot/reimplement2.html
+## Ex 7-4. 이벤트 핸들러 재구성하기2.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        x = 0
+        y = 0
+
+        self.text = f'x: {x}, y: {y}'
+        self.label = QLabel(self.text, self)
+        self.label.move(20, 20)
+
+        self.setMouseTracking(True)
+
+        self.setWindowTitle('Reimplementing event handler')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    def mouseMoveEvent(self, e):
+        x = e.x()
+        y = e.y()
+
+        text = f'x: {x}, y: {y}'
+        self.label.setText(text)
+        self.label.adjustSize()
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
